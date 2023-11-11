045b929745e50d63aff82c89446327e726445f64
Razvan Stefan <144130166+razvan038@users.noreply.github.com>
2023-11-11 15:15:03 +0200

    Update script.js

--- TwoGoodCo/script.js
+++ TwoGoodCo/script.js
@@ -1,11 +1,13 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true,
-    multiplier: 2.5
-});
+  const scroll = new LocomotiveScroll({
+      el: document.querySelector('#main'),
+      smooth: true,
+      multiplier: 0.8
+     });
+
 
 function videoconAnimation() {
 var videocon = document.querySelector("#video-container");
+
 var playbtn = document.querySelector("#play");
 videocon.addEventListener("mouseenter", function(){
     gsap.to(playbtn, {
@@ -21,8 +23,8 @@ videocon.addEventListener("mouseleave", function(){
 });
 videocon.addEventListener("mousemove", function(dets){
     gsap.to(playbtn, {
-        left: dets.x - 250,
-        top: dets.y - 250,
+        left: dets.x - 70,
+        top: dets.y - 70,
     });
 });
 
@@ -30,18 +32,55 @@ videocon.addEventListener("mousemove", function(dets){
 
 function loadingAnimation(){
     gsap.from("#page1 h1", {
-        y:100,
+        y:30,
         opacity:0,
         delay: 0.5,
         duration: 0.5,
         stagger: 0.2,
     });
      gsap.from("#page1 #video-container", {
+         y:30,
+         scale:1,
          opacity:0,
-         delay: 0.5,
+         delay: 0.7,
          duration: 0.5,
      });
 }
 
 videoconAnimation();
 loadingAnimation();
+
+function cursorAnimation() {
+    document.addEventListener("mousemove", function (dets) {
+      gsap.to("#cursor", {
+        left: dets.x,
+        top: dets.y,
+      });
+    });
+    // document.querySelector("#child1").addEventListener("mouseenter",function(){
+  
+    // })
+  
+    // document.querySelector("#child1").addEventListener("mouseleave",function(){
+    //   gsap.to("#cursor",{
+    //     transform: 'translate(-50%,-50%) scale(0)'
+    //   })
+    // })
+    document.querySelectorAll(".child").forEach(function (elem) {
+      elem.addEventListener("mouseenter", function () {
+        gsap.to("#cursor", {
+          transform: "translate(-50%,-50%) scale(1)",
+        });
+      });
+      elem.addEventListener("mouseleave", function () {
+        gsap.to("#cursor", {
+          transform: "translate(-50%,-50%) scale(0)",
+        });
+      });
+    });
+  }
+  cursorAnimation();
+
+
+
+
